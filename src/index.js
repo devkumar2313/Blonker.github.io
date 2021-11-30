0306cf9a0cf0ee3a285c881dae0b68a29f0f8b9d
dev <devkumar333@gmail.com>
2021-11-30 12:30:22 +0530

    git push

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import {BrowserRouter} from 'react-router-dom';
+
+ReactDOM.render((
+   <BrowserRouter basename={process.env.PUBLIC_URL}>
+     <App />
+   </BrowserRouter>,
   document.getElementById('root')
-);
+));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
